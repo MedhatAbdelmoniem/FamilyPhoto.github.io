41dd0665a45f48f47a3a81ab9bb9a57e327b3ecb
MedhatAbdelmoniem <87497679+MedhatAbdelmoniem@users.noreply.github.com>
2021-08-15 15:58:53 +0200

    Add files via upload

---
+++ Family.js
@@ -0,0 +1,33 @@
+var nam = document.getElementById('name')
+var des = document.getElementById('description')
+var photo = document.getElementById('photo')
+var content = document.getElementById('content')
+
+
+document.getElementById('add').addEventListener('click',()=>{
+    if(photo.value == '' || nam.value == '' || des.value == ''){
+        alert('Please enter all data')
+    }else{
+        document.getElementById('temp').style.display = "none"
+        var reader = new FileReader()
+        var pName =  document.createElement('p')
+        var newName = document.createTextNode(nam.value)
+        var pDes = document.createElement('p')
+        var newDes = document.createTextNode(des.value)
+        var newPhoto = document.createElement("img");
+        reader.readAsDataURL(photo.files[0])
+        reader.addEventListener('load',()=>{
+        newPhoto.src = reader.result
+        })
+        pName.appendChild(newName)
+        pName.id = "nameContent"
+        pDes.appendChild(newDes)
+        pDes.id = "desContent"
+        newPhoto.id = "photoContent"
+        content.appendChild(newPhoto)
+        content.appendChild(pName)
+        content.appendChild(document.createElement("br"))
+        content.appendChild(pDes)
+        content.appendChild(document.createElement("hr"))
+    }
+})
